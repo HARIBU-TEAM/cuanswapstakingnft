5d1d55c7966cd85a02ab3fb862318134ef44410e
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2024-01-28 01:17:48 +0800

    install

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { useAddress, useContract, Web3Button } from "@thirdweb-dev/react";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  const address = useAddress();
+
+  const cuanswapstakingnft = "";
+  const stakingaddress = "";
+
+  const { contract: Cuanswapcontract } = useContract (cuanswapstakingnft, "nft-drop");
+  const { contract: stakingContract } = useContract (stakingaddress, "staking");
+  return (
+    <main className={styles.main}>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>CUANSWAP STAKING NFT</h1>
+      </div>
+      <Web3Button contractAddress={cuanswapstakingnft} action={(cuanswapcontract) => cuanswapcontract.erc721.claim(1)}>Claim Cuan NFT</Web3Button>
+    </div>
+  </main>
+  );
+}
+
+export default Home;
