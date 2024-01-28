378619234f46fff87d46ce7f3eeab12d9fb52a9d
masysz <aarel225@gmail.com>
2024-01-28 21:25:25 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { NextPage } from "next";
 const Home: NextPage = () => {
   const address = useAddress();
 
-  const cuanswapstakingnft = "";
+  const cuanswapstakingnft = "0x31A6655746D4C6cbF1f92443e3139323d2f28d54";
   const stakingaddress = "";
 
   const { contract: Cuanswapcontract } = useContract (cuanswapstakingnft, "nft-drop");
