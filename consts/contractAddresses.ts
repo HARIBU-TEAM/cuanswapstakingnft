b330afa1a8add1068a23e6bf6d06463b5027c309
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2024-02-09 20:58:05 +0800

    NFT FOUR

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -4,11 +4,17 @@ export const editionDropContractAddressTwo =
   "0xaaDF63E510fdf55420F3255FF432bcBBa11A035a";
 export const editionDropContractAddressThree =
   "0x29BbDc3e813C0e35E33222534d6493043AFb7C9c";
+export const editionDropContractAddressFour =
+  "0x8512162Cf9d0175DeB52655A9bB62698757E34Cc";
+
 export const tokenContractAddress =
   "0x33Ddf4b5E19b7Cbe69410CFaA2B69aE1F9eBcAD8";
+
 export const stakingContractAddressOne =
   "0xB333A7825885c5722B6BAC29c5991B1dD272865b";
 export const stakingContractAddressTwo =
   "0xd70352202D2972Eb53AF8182950F8B45DB24A7a4";
 export const stakingContractAddressThree =
   "0x91eE4b3616965383EfF3dA9b9041C44612064918";
+export const stakingContractAddressFour =
+  "0x8A244D55070C3C7BF92509c7Ca786AE7E3149842";
